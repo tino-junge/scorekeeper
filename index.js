907590f2885eb114e553cb9cbc212d0d844a755b
Tino Junge <tino.junge@student.hpi.uni-potsdam.de>
2015-08-21 08:35:36 +0200

    Add second player and round numbers

--- index.js
+++ index.js
@@ -7,27 +7,38 @@ class PlayerScore extends Component {
     const { score } = this.props;
     return (
       <li>
-        <div>{score.current} | {score.total}</div>
+        <div>{score.round}: {score.points} | {score.total}</div>
       </li>
     );
   }
 }
 
-// React component
-class Scorekeeper extends Component {
-  render(){
-    const { playerScores, handleSubmit } = this.props;
+const players = [
+  {
+    id: 0,
+    name: 'Player 1'
+  },
+  {
+    id: 1,
+    name: 'Player 2'
+  }
+]
+
+class Player extends Component {
+  render() {
+    const { player, playerScores, handleSubmit } = this.props;
+
     return (
-      <div>
-        <div> Player 1 </div>
+      <div style={{ float: 'left', padding: '20px' }}>
+        <div> {player.name} </div>
         <div>
           <input type="number" defaultValue="0" ref='input' />
-          <button onClick={(e) => this.handleAddPoints(e)}>
+          <button onClick={(e) => this.handleAddPoints(e, player.id)}>
             Enter
           </button>
         </div>
         <div>
-        <div>Current | Total</div>
+        <p>Round | Points | Total</p>
         <ul className='score-list'>
           {playerScores.map(score =>
             <PlayerScore key={score.id} score={score} />
@@ -37,38 +48,58 @@ class Scorekeeper extends Component {
       </div>
     );
   }
-  handleAddPoints(e) {
+  handleAddPoints(e, playerId) {
     const node = findDOMNode(this.refs.input);
     const points = node.value;
-    this.props.onAddPoints(points);
+    this.props.onAddPoints(points, playerId);
+  }
+}
+
+// React component
+class Scorekeeper extends Component {
+  render(){
+    const { players, playerScores, onAddPoints } = this.props;
+    return (
+      <div>
+        {players.map(player =>
+          <Player
+            key={player.id}
+            player={player}
+            onAddPoints={onAddPoints}
+            playerScores={playerScores.filter(score => score.playerId == player.id)}
+          />
+        )}
+      </div>
+    );
   }
 }
 
 Scorekeeper.propTypes = {
+  players: PropTypes.array.isRequired,
   onAddPoints: PropTypes.func.isRequired
 };
 
 // Action:
-function pointsAction(points) {
-  return {type: 'increase', points};
+function pointsAction(points, playerId) {
+  return {type: 'increase', points, playerId};
 }
 
 // Reducer:
-const initialState = [{
-  id: 0,
-  current: 0,
-  total: 0,
-}];
-
-function playerScores(state = initialState, action) {
-  var points = parseInt(action.points);
-  let total = state[0].total;
+function playerScores(state = [], action) {
+  var currentPoints = parseInt(action.points);
+  let scores = state.filter(score => score.playerId == action.playerId);
+  let total = scores.reduce((count, score) =>
+      count + score.points,
+      0
+    );
   switch(action.type){
     case 'increase':
       return [{
         id: state.length,
-        current: points,
-        total: total + points
+        playerId: action.playerId,
+        points: currentPoints,
+        round: scores.length + 1,
+        total: total + currentPoints
       }, ...state];
 
     default:
@@ -82,14 +113,15 @@ let store = createStore(playerScores);
 // Map Redux state to component props
 function mapStateToProps(state)  {
   return {
-    playerScores: state
+    playerScores: state,
+    players: players
   };
 }
 
 // Map Redux actions to component props
 function mapDispatchToProps(dispatch) {
   return {
-    onAddPoints: (points) => dispatch(pointsAction(points))
+    onAddPoints: (points, playerId) => dispatch(pointsAction(points, playerId))
   };
 }
 
