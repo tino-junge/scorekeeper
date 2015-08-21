5bc401f76b73df61fe88abe938c85c7a9b12eaa9
Tino Junge <tino.junge@student.hpi.uni-potsdam.de>
2015-08-21 10:38:08 +0200

    Extract Scorekeeper container in separate file

--- index.js
+++ index.js
@@ -1,48 +1,7 @@
 import React from 'react';
-import { createStore } from 'redux';
-import { Provider, connect } from 'react-redux';
-import Scorekeeper from './components/Scorekeeper';
-import playerScores from './reducers/scorekeeper';
-import pointsAction from './actions/scorekeeper';
-
-// Store:
-const players = [
-  {
-    id: 0,
-    name: 'Player 1'
-  },
-  {
-    id: 1,
-    name: 'Player 2'
-  }
-]
-
-let store = createStore(playerScores);
-
-// Map Redux state to component props
-function mapStateToProps(state)  {
-  return {
-    playerScores: state,
-    players: players
-  };
-}
-
-// Map Redux actions to component props
-function mapDispatchToProps(dispatch) {
-  return {
-    onAddPoints: (points, playerId) => dispatch(pointsAction(points, playerId))
-  };
-}
-
-// Connected Component:
-let App = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Scorekeeper);
+import ScorekeeperApp from './containers/ScorekeeperApp';
 
 React.render(
-  <Provider store={store}>
-    {() => <App />}
-  </Provider>,
+  <ScorekeeperApp />,
   document.getElementById('root')
 );
